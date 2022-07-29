b4ec26c30b8bd2315c38699188362e0682c5ae0b
buukhalai <mbuukhalai@gmail.com>
2022-07-29 20:14:38 +0200

    Project1

---
+++ Test/Test/Bottom.swift
@@ -0,0 +1,33 @@
+//
+//  Bottom.swift
+//  Test
+//
+//  Created by Mamadaminova Buukhalai on 29/7/22.
+//
+
+import SwiftUI
+
+struct Bottom: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Termin anfangen")
+                        .frame(width: 360, height: 30)
+                        .foregroundColor(.white).font(.title2)
+                        .padding()
+                        .background(Color(#colorLiteral(red: 0.8784313725, green: 0.6705882353, blue: 0.09411764706, alpha: 1)))
+                        .cornerRadius(10)
+                    
+                })
+            }
+        }
+    }
+}
+
+struct Top_Previews: PreviewProvider {
+    static var previews: some View {
+        Bottom()
+    }
+}
+
---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Mamadaminova Buukhalai on 29/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
